857583ca0e5777670354f0971571121a8d13a075
sherlock <git@aaditagrawal.com>
2025-05-18 14:46:52 +0530

    Added GitHub Workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://energy-template.vbartalis.dev",
-  base: "",
+  site: "https://madtalesai.github.io",
+  base: "/madtales.ai_web",
 });
